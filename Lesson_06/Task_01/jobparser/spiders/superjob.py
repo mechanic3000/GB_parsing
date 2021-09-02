e354b65567a467b029fe1b8d7370ddc86fe438d8
Alexey Illarionov <alx.illarionov@gmail.com>
2021-09-02 14:25:08 +0300

    shorter xpath

--- Lesson_06/Task_01/jobparser/spiders/superjob.py
+++ Lesson_06/Task_01/jobparser/spiders/superjob.py
@@ -10,7 +10,7 @@ class SuperjobSpider(scrapy.Spider):
                   'https://www.superjob.ru/vacancy/search/?keywords=python&geo%5Br%5D%5B0%5D=2']
 
     def parse(self, response: HtmlResponse):
-        urls = response.xpath("//div[contains(@class,'f-test-vacancy-item')]/div/div[3]/div/div/div/a/@href").getall()
+        urls = response.xpath("//div[contains(@class,'f-test-vacancy-item')]//a[contains(@href, 'vakansii')]/@href").getall()
         next_page_button = response.xpath("//a[contains(@class,'f-test-button-dalshe')]/@href").get()
         if next_page_button:
             yield response.follow(next_page_button, callback=self.parse)
@@ -19,7 +19,7 @@ class SuperjobSpider(scrapy.Spider):
 
     def vacancy_parse(self, response: HtmlResponse):
         name = response.xpath("//h1/text()").get()
-        salary = response.xpath("//h1/following-sibling::span/span/span//text()").getall()
+        salary = response.xpath("//h1/following-sibling::span//text()").getall()
         url = response.url
         site_from = SuperjobSpider.allowed_domains[0]
 
