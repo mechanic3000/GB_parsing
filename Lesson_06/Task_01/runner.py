80307e5a45ead03c9a9cff04d9cfd8b78b66d2c2
Alexey Illarionov <alx.illarionov@gmail.com>
2021-09-02 14:25:25 +0300

    shorter xpath

--- Lesson_06/Task_01/runner.py
+++ Lesson_06/Task_01/runner.py
@@ -2,7 +2,7 @@ from scrapy.settings import Settings
 from scrapy.crawler import CrawlerProcess
 
 from Lesson_06.Task_01.jobparser import settings
-# from Lesson_06.Task_01.jobparser.spiders.hhru import HhruSpider
+from Lesson_06.Task_01.jobparser.spiders.hhru import HhruSpider
 from Lesson_06.Task_01.jobparser.spiders.superjob import SuperjobSpider
 
 if __name__ == '__main__':
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     crawler_setting.setmodule(settings)
 
     process = CrawlerProcess(crawler_setting)
-    # process.crawl(HhruSpider)
+    process.crawl(HhruSpider)
     process.crawl(SuperjobSpider)
 
     process.start()
